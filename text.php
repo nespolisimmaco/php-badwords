d7750e526f81dd39bf260fe6c9838a81818507fa
nespolisimmaco <snespoli@yahoo.com>
2023-05-03 18:06:43 +0200

    Add: print entered text with its length and censored text with its length

--- text.php
+++ text.php
@@ -7,6 +7,26 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php 
+    $text = $_GET['text'];
+    $bad_word = $_GET['word'];
+    ?>
+
+    <!-- Testo inserito -->
+    <h2>Testo inserito</h2>
+    <p>
+        <?php echo $text; ?> 
+    </p>
+    <h4>Lunghezza testo: <?php echo strlen($text); ?></h4>
+
+    <!-- Testo inserito con parola censurata -->
+    <h2>Testo con parola censurata</h2>
+    <p>
+        <?php 
+        $censored_text = str_replace($bad_word, '***', $text);
+        echo $censored_text;
+        ?>
+    </p>
+    <h4>Lunghezza testo censurato: <?php echo strlen($censored_text); ?></h4>
 </body>
 </html>
